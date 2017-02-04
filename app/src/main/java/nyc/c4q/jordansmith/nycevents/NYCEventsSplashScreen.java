34c6af7c46cceebf5d68149ea4ddb4fde02fa2e7
tking2005 <tking2005@gmail.com>
2017-02-04 14:29:43 -0500

    updated Splash Screen

--- app/src/main/java/nyc/c4q/jordansmith/nycevents/NYCEventsSplashScreen.java
+++ app/src/main/java/nyc/c4q/jordansmith/nycevents/NYCEventsSplashScreen.java
@@ -1,13 +1,44 @@
 package nyc.c4q.jordansmith.nycevents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class NYCEventsSplashScreen extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nycevents_splash_screen);
+
+        imageView = (ImageView)findViewById(R.id.imageView_skyscrapper_background);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.explore_nyc_welcome);
+        imageView.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
     }
+
 }
